e303d9283e22bf051306e978570d7a5f12b4ee8d
gautric <gregoire@autric.net>
2013-05-21 23:26:12 +0200

    update

--- src/fr/redhat/testing/TestSLF4j.java
+++ src/fr/redhat/testing/TestSLF4j.java
@@ -1,15 +1,17 @@
 package fr.redhat.testing;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 public class TestSLF4j {
-	  private static final Logger LogTech = Logger.getLogger("tech."+TestSLF4j.class.getCanonicalName());
-	  private static final Logger LogFonc = Logger.getLogger(TestSLF4j.class.getCanonicalName());
-	  
-	  
-	  public static void main(String[] args) {
+	private static final Logger LogTech = LoggerFactory.getLogger("tech."
+			+ TestSLF4j.class.getCanonicalName());
+	private static final Logger LogFonc = LoggerFactory.getLogger(TestSLF4j.class
+			.getCanonicalName());
+
+	public static void main(String[] args) {
 		LogTech.error("Mon Erreur Tech");
 		LogFonc.error("Mon Erreur Fonc");
 	}
